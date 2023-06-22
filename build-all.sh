10b25a1c9908bc647005be1d3e440780f44edd0d
gitlab.com/pepa65 <pepa65@passchier.net>
2023-06-22 13:07:25 +0700

    Song Book files

--- build-all.sh
+++ build-all.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
 # build-all.sh - Make all the files in the project
+# Required: a5toa4 [https://gitlab.com/pepa65/misc]
 
-echo 'making OpenLP/*.xml files'
+echo 'Making OpenLP/*.xml files'
 ./openlp.sh
 
-echo 'making worship.html'
+echo 'Making worship.html'
 ./worship.sh
 
-echo 'making worshipp.html'
+echo 'Making worshipp.html'
 ./worshipp.sh
 
-echo 'making worship.pdf and worship2.pdf'
+echo 'Making worship.pdf and worship2.pdf'
 ./pdf.sh
+
+echo 'Making Song Book files'
+./songbook.sh
 
 echo 'making BookletA4.pdf'
 a5toa4 BookletA5.pdf BookletA4.pdf
