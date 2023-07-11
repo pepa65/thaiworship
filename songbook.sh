766c959e3e9cfcb92b9685c0fd00c68825978544
gitlab.com/pepa65 <pepa65@passchier.net>
2023-07-12 01:48:36 +0700

    Songbook tweaks

--- songbook.sh
+++ songbook.sh
@@ -2,12 +2,12 @@
 
 # songbook.sh - Make separate png files of each song for Song Book
 #
-# Produce png files based on pdf files based on html files for each song,
+# Produce png files [based on pdf files] based on html files for each song,
 # and the json file, for online use in Song Book (Soli Deo Gloria app).
 #
 # Input: $songs_file (song content), expected in the same directory.
 # Output: $out/*.png $json (id,title,lyricstype,lyrics,[audiourl,]category)
-#         $out/*.pdf and $out/*.htm are intermediate products
+#         [$out/*.pdf and] $out/*.htm are intermediate products
 #
 # Usage: songbook.sh [-j|--json]
 #        -j/--json:  Only generate the json file
@@ -16,6 +16,7 @@
 
 out='songbook'
 jsonfile='songs.json'
+pdf=0
 
 [[ $1 = -j || $1 = --json ]] && json=1 || json=0
 self=$(readlink -e "$0")
@@ -30,21 +31,38 @@ h1='+'  # Section Header
 c1='#'  # Comment
 nl=$'\n'  # Newline
 
-if ! wp=$(type -P weasyprint)
-then # WeasyPrint not found in PATH
-  echo "Cannot make pdf files, WeasyPrint not installed, see:"
-  echo " http://weasyprint.readthedocs.io/en/latest/install.html"
-  echo "Either do:"
-	echo " sudo apt install weasyprint"
-	echo "or:"
-	echo " sudo pip install WeasyPrint"
-  exit 1
-fi
-if ! im=$(type -P convert)
-then # ImageMagick not found in PATH
-  echo "Cannot make png files, ImageMagick not installed, do:"
-	echo " sudo apt install imagemagick"
-  exit 2
+if ((pdf))
+then # Use Weasyprint and convert
+	if ! we=$(type -P weasyprint)
+	then # WeasyPrint not found in PATH
+	  echo "Cannot make pdf files, WeasyPrint not installed, see:"
+	  echo " http://weasyprint.readthedocs.io/en/latest/install.html"
+	  echo "Either do:"
+		echo " sudo apt install weasyprint"
+		echo "or:"
+		echo " sudo pip install WeasyPrint"
+	  exit 1
+	fi
+	if ! co=$(type -P convert)
+	then # ImageMagick not found in PATH
+	  echo "Cannot transform png files, ImageMagick not installed, do:"
+		echo " sudo apt install imagemagick"
+	  exit 2
+	fi
+else # User chromium and mogrify
+	if ch=$(type -P chromium) || ch=$(type -P chrome)
+	then : # OK
+	else # neither chromium nor chrome found in PATH
+	  echo "Cannot make png files, chromium/chrome not installed, do:"
+		echo " sudo apt install chromium"
+	  exit 3
+	fi
+	if ! mo=$(type -P mogrify)
+	then # ImageMagick not found in PATH
+	  echo "Cannot transform png files, ImageMagick not installed, do:"
+		echo " sudo apt install imagemagick"
+	  exit 4
+	fi
 fi
 
 ((json)) || rm -rf -- "$out"
@@ -62,9 +80,14 @@ do # Process $songs_file line
 		then # Finish previous title
 			if ((!json))
 			then
-				echo "</p>" >>"$out/$id.htm"
-				"$wp" -v "$out/$id.htm" "$out/$id.pdf"
-				"$im" -density 120 -depth 3 "$out/$id.pdf" -trim +repage -bordercolor White -border 24 -define png:color-type=3 "$out/$id.png"
+				if ((pdf))
+				then
+					$we -v "$out/$id.htm" "$out/$id.pdf"
+					$co -density 120 -depth 3 "$out/$id.pdf" -trim +repage -bordercolor White -border 24 -define png:color-type=3 "$out/$id.png"
+				else
+					$ch --headless=new --window-size=512,4096 --disable-gpu --enable-chrome-browser-cloud-management --force-device-scale-factor=1 --screenshot="$out/$id.png" "$out/$id.htm"
+					$mo -density 120 -depth 3 -trim +repage -bordercolor White -border 24 -define png:color-type=3 "$out/$id.png"
+				fi
 			fi
 		fi
 		title=$rest id=${rest%% *}
@@ -79,8 +102,9 @@ do # Process $songs_file line
 				i{font-size:90%; font-style:normal; color:#666;}
 				@page{size:299mm 999mm; margin:.4in;}
 				</style>
+				<p><b>$title</b>
+				<p>
 			HEAD
-			echo "<p><b>$title</b></p><p>" >>"$out/$id.htm"
 		fi
 	elif [[ $first = $h1 ]]
 	then # h2 section header
@@ -89,7 +113,7 @@ do # Process $songs_file line
 	then # verse separator
 		if ((!json))
 		then
-			echo "</p><p>" >>"$out/$id.htm"
+			echo "<p>" >>"$out/$id.htm"
 			[[ $rest ]] && echo "$(sed -e 's@\[@<i>[@g' -e 's@]@]</i>@g' <<<"$rest")<br>" >>"$out/$id.htm"
 		fi
 	else # songline -- if not empty
@@ -102,9 +126,14 @@ done <"$songs_file"
 # Finish last title
 if ((!json))
 then
-	echo "</p>" >>"$out/$id.htm"
-	"$wp" -v "$out/$id.htm" "$out/$id.pdf"
-	"$im" -density 120 -depth 3 "$out/$id.pdf" -trim +repage -bordercolor White -border 24 -define png:color-type=3 "$out/$id.png"
+	if ((pdf))
+	then
+		$we -v "$out/$id.htm" "$out/$id.pdf"
+		$co -density 120 -depth 3 "$out/$id.pdf" -trim +repage -bordercolor White -border 24 -define png:color-type=3 "$out/$id.png"
+	else
+		$ch --headless=new --window-size=512,4096 --disable-gpu --enable-chrome-browser-cloud-management --force-device-scale-factor=1 --screenshot="$out/$id.png" "$out/$id.htm"
+		$mo -density 120 -depth 3 -trim +repage -bordercolor White -border 24 -define png:color-type=3 "$out/$id.png"
+	fi
 fi
 
 echo -e "${jsonstr:0: -3}]" >"$jsonfile"
