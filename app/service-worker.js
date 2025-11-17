4e2743d83cfadc76a4d9e7a0b8e1e9ed2e1d96b8
gitlab.com/pepa65 <pepa65@passchier.net>
2025-11-17 07:11:58 +0700

    Finally

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME='thaiworship-cache';
-const urlsToCache=['/', '/index.html', '/styles.css', 'android-chrome-192x192.png', 'android-chrome-512x512.png', 'apple-touch-icon.png', 'favicon-16x16.png', 'favicon-32x32.png', 'favicon.ico', 'maskable_icon.png', 'safari-pinned-tab.svg'];
+const urlsToCache=['/', '/index.html', 'android-chrome-192x192.png', 'android-chrome-512x512.png', 'apple-touch-icon.png', 'favicon-16x16.png', 'favicon-32x32.png', 'favicon.ico', 'maskable_icon.png', 'safari-pinned-tab.svg'];
 self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open(CACHE_NAME)
